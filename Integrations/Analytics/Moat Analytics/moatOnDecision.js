2551ff363275a49ef5bbbad5eafa734c7860d6ae
Danny-Driscoll <daniel.driscoll@optimizely.com>
2017-07-13 18:30:51 -0400

    Update moatOnDecision.js

--- Integrations/Analytics/Moat Analytics/moatOnDecision.js
+++ Integrations/Analytics/Moat Analytics/moatOnDecision.js
@@ -1,7 +1,7 @@
 //Fetch Optimizely State
 var state = optimizely.get('state');
 //Get the current Decision String from the state
-var string = state.getDecisionString({"campaignId":campaignId});
+var string = state.getDecisionString({"campaignId":campaignId,"shouldCleanString":true});
 //Check for a truthy value for the string indicating a visitor has been successfully bucketed
 if (string) {
   //create or update the window-level optimizelyMoat array with the Decision String if 
