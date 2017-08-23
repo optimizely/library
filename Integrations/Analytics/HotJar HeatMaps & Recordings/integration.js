bbb302021ba1b8558552931b1e502f3a66eb8636
davidsertillange-optimizely <dsertillange@ams-dsertillan-5575.local>
2017-08-23 11:40:22 +0200

    last commit for HotJar, ready to be PR-ed

--- Integrations/Analytics/HotJar HeatMaps & Recordings/integration.js
+++ Integrations/Analytics/HotJar HeatMaps & Recordings/integration.js
@@ -1,24 +1,27 @@
 // We define the HotJar object if this has not been done already
 window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
 
+// We get the decision in a string
 var decisionString = window.optimizely.get("state").getDecisionString({
   campaignId: campaignId,
   shouldCleanString:true,
   maxLength:50
  });
 
-console.log('decisionString');
-console.log(decisionString);
-
-// We trigger the heatmap for this variation
 if(extension.heatmaps === "true") {
-  hj('trigger', extension.javascript_trigger+'_'+variationId);
-  console.log('HJ Javascript trigger', extension.javascript_trigger);
+  // If the visitor is in the holdback
+  if(isHoldback === true) {
+    // We trigger the heatmap
+    hj('trigger', extension.javascript_trigger+'_holdback');
+  } else {
+    // We trigger the heatmap
+    hj('trigger', extension.javascript_trigger+'_'+variationId);
+  }
 }
 
-if(extension.recordings === "false") {
+if(extension.recordings === "true") {
   // We tag recordings with experiment info
-  hj('tagRecording', [DecisionString]);
+  hj('tagRecording', [decisionString]);
 }
 
 
