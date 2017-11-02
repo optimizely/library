2c02e6bd7e624b2086b58872bbe8cc79fd8a19b5
cooperreid-optimizely <cooper.reid@optimizely.com>
2017-11-02 17:09:40 -0400

    refactored a bit.

--- Integrations/Analytics/Adobe Analytics/List Variable Integration/integration.js
+++ Integrations/Analytics/Adobe Analytics/List Variable Integration/integration.js
@@ -1,41 +1,62 @@
-// This extension integrates Optimizely w/ Adobe via a pre-defined list variable. Set the list variable on line 3 below.
-var decisionString = window.optimizely.get('state').getDecisionString({campaignId: campaignId, shouldCleanString: true});
+var decisionString = window.optimizely.get('state').getDecisionString({
+  campaignId: campaignId,
+  shouldCleanString: true
+});
 var redirectInfo = window.optimizely.get('state').getRedirectInfo();
-var list = "list1";
+
+var csvToArray = function(csv) {
+  return csv.length ? (csv || '').split(/,\s*/) : [];
+}
 
 // Public Methods
 var adobeIntegrator = {
-	// Array of objects for active campaigns/experiments
-	campaignArray: [],
-	// Accepts "s" variable as a param and assigns campaigns/experiments to list.
-	assignCampaigns: function(sVariable) {
-    sVariable[list] = sVariable[list] || [];
+  campaignDecisions: [],
+  appendToProperty: function(sVariable, listVar, decision) {
+    if (typeof sVariable[listVar] === 'undefined') sVariable[listVar] = ''; //default to string format
+    
+    if (sVariable[listVar].constructor === Array) { // this listVarVar was previously defined as a string
+      sVariable[listVar] = sVariable[listVar].concat(decision);
+    } 
+    else { // this listVarVar was previously defined as a string
+      sVariable[listVar] = csvToArray(sVariable[listVar]);
+      sVariable[listVar].push(decision);
+      sVariable[listVar] = sVariable[listVar].join(',');
+    }
+  },
+  populateTrackerClearDecisions: function(sVariable) {
+    var _this = this;
+    this.campaignDecisions.forEach(function(i) {
+      _this.appendToProperty(sVariable, i.listVar, i.decision);
+    });
+    this.campaignDecisions = [];
+  },
+  // Accepts "s" variable as a param and assigns eVars to object.  
+  assignCampaigns: function(sVariable) {
     if (!!redirectInfo) sVariable.referrer = redirectInfo.referrer;
-		for (var i = this.campaignArray.length-1; i >= 0; i--) {
-    		sVariable[list].push(this.campaignArray[i]);
-			this.campaignArray.splice(i, 1);
-		}
-	},
-	// Accepts "s" variable as a param and assigns eVars to object, then dispatches custom link tracking.
-	trackDelayedCampaigns: function(sVariable) {
-    sVariable[list] = sVariable[list] || [];
-    sVariable.linkTrackVars += ",list1";
-		for (var i = this.campaignArray.length-1; i >= 0; i--) {
-			sVariable[list].push(this.campaignArray[i]);
-			this.campaignArray.splice(i, 1);
-		}
+    this.populateTrackerClearDecisions(sVariable);
+  },
+  // Accepts "s" variable as a param and assigns eVars to object, then dispatches custom link tracking.
+  trackDelayedCampaigns: function(sVariable) {
+    var _this = this;
+    this.campaignDecisions.forEach(function(i) {
+      _this.appendToProperty(sVariable, 'linkTrackVars', i.listVar);
+    });    
+    this.populateTrackerClearDecisions(sVariable);
     sVariable.tl(true, "o", "OptimizelyLayerDecision");
-	}
+  }
 };
 
 // Scopes `campaignArray` to Optimizely object.
 if (!window.optimizely.get("custom/adobeIntegrator")) {
-	window.optimizely.push({
-		type: "registerModule",
-		moduleName: "adobeIntegrator",
-		module: adobeIntegrator
-	});
+  window.optimizely.push({
+    type: "registerModule",
+    moduleName: "adobeIntegrator",
+    module: adobeIntegrator
+  });
 }
 
 // Failing Audiences returns `null`, failing Traffic Allocation returns `undefined` for decisionString.
-if (!!decisionString) window.optimizely.get("custom/adobeIntegrator").campaignArray.push(decisionString);
+if (!!decisionString) window.optimizely.get("custom/adobeIntegrator").campaignDecisions.push({
+  'listVar': extension.list,
+  'decision': decisionString
+});
