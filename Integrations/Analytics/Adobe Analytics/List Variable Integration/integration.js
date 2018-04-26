f83e3dcdf171d1bc8a30a07c1887ea93a60f91d9
cooperreid-optimizely <cpreid>
2018-04-26 10:52:10 -0400

    made listvar reference the variable

--- Integrations/Analytics/Adobe Analytics/List Variable Integration/integration.js
+++ Integrations/Analytics/Adobe Analytics/List Variable Integration/integration.js
@@ -1,40 +1,43 @@
 // This extension integrates Optimizely w/ Adobe via a pre-defined list variable. Set the list variable on line 3 below.
-var decisionString = window.optimizely.get('state').getDecisionString({campaignId: campaignId, shouldCleanString: true});
+var decisionString = window.optimizely.get('state').getDecisionString({
+  campaignId: campaignId,
+  shouldCleanString: true
+});
 var redirectInfo = window.optimizely.get('state').getRedirectInfo();
 var list = "list1";
 
 // Public Methods
 var adobeIntegrator = {
-	// Array of objects for active campaigns/experiments
-	campaignArray: [],
-	// Accepts "s" variable as a param and assigns campaigns/experiments to list.
-	assignCampaigns: function(sVariable) {
+  // Array of objects for active campaigns/experiments
+  campaignArray: [],
+  // Accepts "s" variable as a param and assigns campaigns/experiments to list.
+  assignCampaigns: function(sVariable) {
     sVariable[list] = sVariable[list] || [];
     if (!!redirectInfo) sVariable.referrer = redirectInfo.referrer;
-		for (var i = this.campaignArray.length-1; i >= 0; i--) {
-    		sVariable[list].push(this.campaignArray[i]);
-			this.campaignArray.splice(i, 1);
-		}
-	},
-	// Accepts "s" variable as a param and assigns eVars to object, then dispatches custom link tracking.
-	trackDelayedCampaigns: function(sVariable) {
+    for (var i = this.campaignArray.length - 1; i >= 0; i--) {
+      sVariable[list].push(this.campaignArray[i]);
+      this.campaignArray.splice(i, 1);
+    }
+  },
+  // Accepts "s" variable as a param and assigns eVars to object, then dispatches custom link tracking.
+  trackDelayedCampaigns: function(sVariable) {
     sVariable[list] = sVariable[list] || [];
-    sVariable.linkTrackVars += ",list1";
-		for (var i = this.campaignArray.length-1; i >= 0; i--) {
-			sVariable[list].push(this.campaignArray[i]);
-			this.campaignArray.splice(i, 1);
-		}
+    sVariable.linkTrackVars += ("," + list);
+    for (var i = this.campaignArray.length - 1; i >= 0; i--) {
+      sVariable[list].push(this.campaignArray[i]);
+      this.campaignArray.splice(i, 1);
+    }
     sVariable.tl(true, "o", "OptimizelyLayerDecision");
-	}
+  }
 };
 
 // Scopes `campaignArray` to Optimizely object.
 if (!window.optimizely.get("custom/adobeIntegrator")) {
-	window.optimizely.push({
-		type: "registerModule",
-		moduleName: "adobeIntegrator",
-		module: adobeIntegrator
-	});
+  window.optimizely.push({
+    type: "registerModule",
+    moduleName: "adobeIntegrator",
+    module: adobeIntegrator
+  });
 }
 
 // Failing Audiences returns `null`, failing Traffic Allocation returns `undefined` for decisionString.
