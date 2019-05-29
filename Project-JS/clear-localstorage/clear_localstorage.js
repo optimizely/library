fec462954278658b324c308454ac834ea57b0aa4
Asa Schachar <asa@optimizely.com>
2019-05-29 08:32:16 -0700

    fix (project-js): Ensure method name is correct
    - Ensure method name `clearOptlyLocalStorage` is correct.
    - Update styling

--- Project-JS/clear-localstorage/clear_localstorage.js
+++ Project-JS/clear-localstorage/clear_localstorage.js
@@ -1,14 +1,16 @@
-/*Exposing a window-level function to clear localStorage values who's key includes the substring 'optimizely' 
-*/
-window.clearOptlyLocalStorage = function(){
-    for (var key in window.localStorage){
-        if(key.indexOf('optimizely') !== -1){
-            window.localStorage.removeItem(key)
-        }
+/**
+ * Exposing a window-level function to clear localStorage values who's key includes the substring 'optimizely' 
+ */
+window.clearOptlyLocalStorage = function() {
+  for (var key in window.localStorage) {
+    if (key.indexOf('optimizely') !== -1) {
+      window.localStorage.removeItem(key)
     }
+  }
 }
-//The function can be called from Optimizely Project JS based on an available boolean flag, such as a cookie's value 
+
+// The function can be called from Optimizely Project JS based on an available boolean flag, such as a cookie's value 
 var should_clear = window.customer_storage_flag_boolean;
-if(should_clear){
-    clearOptimizelyLocalStorage();
+if (should_clear) {
+  clearOptlyLocalStorage();
 }
