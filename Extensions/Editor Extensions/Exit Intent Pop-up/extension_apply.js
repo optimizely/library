11a3b857fd2c87625a88bb16f970bfe023f50d8c
Asa Schachar <asa@optimizely.com>
2019-08-22 15:37:13 -0700

    Replace all tabs with spaces

--- Extensions/Editor Extensions/Exit Intent Pop-up/extension_apply.js
+++ Extensions/Editor Extensions/Exit Intent Pop-up/extension_apply.js
@@ -1,17 +1,17 @@
 var utils = window.optimizely.get('utils');
 
 utils.waitForElement('body').then(function(element){
-	var html = widget.$html;
+  var html = widget.$html;
   element.insertAdjacentHTML('afterbegin', html);
   element.addEventListener('mouseleave', function(){
     showPopup();
-		bindCloseBtn();
+    bindCloseBtn();
   });
 });
 
 function showPopup() {
-  if(document.querySelector('.fade-out')){
-	  document.querySelector('.fade-out').classList.remove('fade-out');
+  if (document.querySelector('.fade-out')) {
+    document.querySelector('.fade-out').classList.remove('fade-out');
   }
   document.querySelector('.exit-intent-modal').classList += ' fade-in';
 }
@@ -22,7 +22,7 @@ function closePopup() {
 }
 
 function bindCloseBtn() {
-	document.querySelector('.exit-popup-close').addEventListener('click', function(e){
+  document.querySelector('.exit-popup-close').addEventListener('click', function(e) {
     closePopup();
   });
 }
