c738c9923363b01a6a396f1e7d8539e42e17f458
Asa Schachar <asa@optimizely.com>
2019-08-23 10:49:37 -0700

    Update facebook URL

--- Extensions/Editor Extensions/Facebook Messenger Widget - beta/extension_apply.js
+++ Extensions/Editor Extensions/Facebook Messenger Widget - beta/extension_apply.js
@@ -18,7 +18,7 @@ window.fbAsyncInit = function() {
    var js, fjs = d.getElementsByTagName(s)[0];
    if (d.getElementById(id)) {return;}
    js = d.createElement(s); js.id = id;
-   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
    fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'facebook-jssdk'))
 
