89400ca19868b61d1c1a3600d4027bfda6496461
Mark Biesheuvel <mail@markbiesheuvel.nl>
2023-08-11 10:59:43 +0200

    Hotjar integration - Add option to mask long numbers

--- Integrations/Analytics/HotJar HeatMaps & Recordings/integration.js
+++ Integrations/Analytics/HotJar HeatMaps & Recordings/integration.js
@@ -2,11 +2,18 @@
 window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
 
 // We get the decision in a string
-const decisionString = window.optimizely.get('state').getDecisionString({
+let decisionString = window.optimizely.get('state').getDecisionString({
   campaignId: campaignId,
   shouldCleanString:true,
   maxLength:50
 });
+
+if (extension.mask_long_numbers === 'true') {
+  // Mask any sequences of 9 or more digits to avoid PII filter from Hotjar
+  const maskIdRegex = /(\d{3})\d{3,}(\d{3})/g;
+  variationId = variationId.replaceAll(maskIdRegex, '$1...$2');
+  decisionString = decisionString.replaceAll(maskIdRegex, '$1...$2');
+}
 
 if (extension.heatmaps === 'true') {
   // If the visitor is in the holdback
