98425f5d43d83e91680870598b8217fd1fa26db0
Mark Biesheuvel <mail@markbiesheuvel.nl>
2024-08-30 14:56:14 +0200

    Update Amplitude integration to use Amplitude SDK v2

--- Integrations/Analytics/Amplitude/README.md
+++ Integrations/Analytics/Amplitude/README.md
@@ -2,6 +2,4 @@
 
 ## Description
 
-Use this integration to track your Optimizely experiments in Amplitude. 
-
-Please note that this integration is in beta right now, but it is already successfully in use by a few of our customers.
+Use this integration to track your Optimizely experiments in Amplitude.
--- Integrations/Analytics/Amplitude/integration.js
+++ Integrations/Analytics/Amplitude/integration.js
@@ -1,74 +1,56 @@
-// VERSION 0.1.0
-// Last Updated: July 9th 2019
+// VERSION 0.2.0
+// Last Updated: August 30th 2024
 
-var dataSent = false;
-var MAX_ATTEMPTS = 9;
-var RETRY_DELAY_MS = 1000;
+const MAX_ATTEMPTS = 9;
+const RETRY_DELAY_MS = 1000;
 
-function getCampaignInfo() {
- return window.optimizely
- .get("state")
- .getDecisionObject({ campaignId: campaignId });
-}
+const logEvent = ({campaign, experiment, variation, holdback}) => {
+	const eventProperties = {
+		'[Optimizely Campaign]': campaign,
+		'[Optimizely Experiment]': experiment,
+		'[Optimizely Variation]': variation,
+		'[Optimizely Holdback]': holdback
+	};
 
-function logEvent() {
- var campaignInfo = getCampaignInfo();
+	window.amplitude.logEvent(extension.event_name, eventProperties);
+};
 
- if (campaignInfo) {
- var eventProperties = {
- "[Optimizely Campaign]": campaignInfo.campaign,
- "[Optimizely Experiment]": campaignInfo.experiment,
- "[Optimizely Variation]": campaignInfo.variation,
- "[Optimizely Holdback]": campaignInfo.holdback
- };
- amplitude.getInstance(extension.instance_name).logEvent(extension.event_name, eventProperties);
- }
-}
+const identifyCall = ({experiment, variation, holdback}) => {
+	if (!holdback) {
+		const identifyEvent = new window.amplitude.Identify();
+		identifyEvent.set(`${extension.property_prefix} ${experiment}`, variation);
 
-function identifyCall() {
- var campaignInfo = getCampaignInfo();
+		window.amplitude.identify(identifyEvent);
+	}
+};
 
- if (campaignInfo) {
- var identify = new amplitude.Identify().set(
- extension.property_prefix + " " + campaignInfo.experiment,
- campaignInfo.variation
- );
- amplitude.getInstance(extension.instance_name).identify(identify);
- }
-}
+const sendData = () => {
+	const campaignInfo = window.optimizely
+		.get('state')
+		.getDecisionObject({ campaignId });
 
-function sendData() {
- if (!dataSent) {
- identifyCall();
- if (extension.send_event === "y") {
- logEvent();
- }
- }
- dataSent = true;
-}
+	if (campaignInfo) {
+		// Always send identify event
+		identifyCall(campaignInfo);
+		if (extension.send_event === 'y') {
+			// Only log event if so configured
+			logEvent(campaignInfo);
+		}
+	}
+};
 
-function sendToAmplitude(call) {
- if (call >= MAX_ATTEMPTS) {
- return;
- }
- 
- var instanceKey = extension.instance_name || "$default_instance";
+const sendToAmplitude = (numberOfAttempts) => {
+	if (window.amplitude) {
+		// Send data to Amplitude
+		sendData();
+	} else {
+		// Retry until Amplitude is initialized
+		if (numberOfAttempts < MAX_ATTEMPTS) {
+			setTimeout(() => {
+				sendToAmplitude(numberOfAttempts + 1);
+			}, RETRY_DELAY_MS);
+		}
+	}
+};
 
- if (window.amplitude && window.amplitude.getInstance) {
- var instance = window.amplitude.getInstance(extension.instance_name);
- 
- if (instance._isInitialized) {
- return sendData();
- } else if (instance.onInit) {
- instance.onInit(function() {
- sendData();
- });
- return;
- }
- }
- 
- return setTimeout(function() {
- sendToAmplitude(call + 1);
- }, RETRY_DELAY_MS);
-}
 sendToAmplitude(0);
